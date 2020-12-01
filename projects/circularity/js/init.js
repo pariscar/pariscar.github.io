ce77a1ea5745e3bc9ca8b1ad488dea18963d87e5
pariscar <carneyp10@gmail.com>
2020-12-01 20:56:05 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,7 +26,7 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 10, 10);
             view.addChild(circle);
             circles.push(circle);
         }
@@ -48,11 +48,7 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            // physikz.updatePosition( circles[0] );
-	        // physikz.updatePosition( circles[1] );
-	        // physikz.updatePosition( circles[2] );
-	        // physikz.updatePosition( circles[3] );
-	        // physikz.updatePosition( circles[4] );
+
             
             // TODO 5 : Call game.checkCirclePosition() on your circles.
 
@@ -72,20 +68,20 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if (circle.x > canvas.width) {
                 circle.x = 0;
             }
             
             // TODO 5 : YOUR CODE STARTS HERE //////////////////////
-            if ( circle.y > canvas.height ) {
+            if (circle.y > canvas.height) {
                 circle.y = 0;
             }
 
-            if ( circle.x < 0 ) {
+            if (circle.x < 0) {
                 circle.x = canvas.width;
             }
 
-            if ( circle.y < 0 ) {
+            if (circle.y < 0) {
                 circle.y = canvas.height;
             }
 
