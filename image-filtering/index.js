03d7448a5201ac17bb6cb7a958e2159194680316
pariscar <carneyp10@gmail.com>
2021-04-28 22:14:05 +0000

    img fltr

---
+++ image-filtering/index.js
@@ -0,0 +1,35 @@
+// This is a small program. There are only two sections. This first section is what runs
+// as soon as the page loads and is where you should call your functions.
+$(document).ready(function(){
+    const $display = $('#display');
+
+    // TODO: Call your apply function(s) here
+    applyFilter();
+
+
+
+
+    render($display, image);
+});
+
+/////////////////////////////////////////////////////////
+// "apply" and "filter" functions should go below here //
+/////////////////////////////////////////////////////////
+
+// TODO: Create the applyFilter function here
+function applyFilter(){
+    for(var r = 0; r < image.length; r++){
+        for(var c = 0; c < image[r].length; c++){
+            var rgbString = image[1][1];
+            
+        }
+    }
+}
+
+// TODO: Create the applyFilterNoBackground function
+
+
+// TODO: Create filter functions
+
+
+// CHALLENGE code goes below here
