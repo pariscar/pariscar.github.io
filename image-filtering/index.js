5a61780ce1ebdd010dc7331933ac98fe4fc4a3b1
pariscar <carneyp10@gmail.com>
2021-05-05 23:23:19 +0000

    image

--- image-filtering/index.js
+++ image-filtering/index.js
@@ -4,9 +4,11 @@ $(document).ready(function(){
     const $display = $('#display');
 
     // TODO: Call your apply function(s) here
-    applyFilter(reddify);
-
-
+    //applyFilter(reddify);
+    applyFilterNoBackground(reddify);
+    //applyFilterNoBackground(filterFunction);
+    //applyFilter(decreaseBlue);
+    //applyFilter(increaseGreenByBlue);
 
 
     render($display, image);
@@ -20,22 +22,57 @@ $(document).ready(function(){
 function applyFilter(filterFunction){
     for (var r = 0; r < image.length; r++){
         for (var c = 0; c < image[r].length; c++){
-            var rgbString = image[0][0];
+            var rgbString = image[r][c];
             var rgbNumbers = rgbStringToArray(rgbString);
             filterFunction(rgbNumbers);
-            //rgbNumbers[RED] = 255;
             rgbString = rgbArrayToString(rgbNumbers);
-            image[0][0] = rgbString;
+            image[r][c] = rgbString;
         }
     }
 }
 
 // TODO: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction){
+    for (var r = 0; r < image.length; r++){
+        for (var c = 0; c < image[r].length; c++){
+            var rgbString = image[r][c];
+            if (image[r][c] === image[0][0]){
+                
+            }
+            var rgbNumbers = rgbStringToArray(rgbString);
+            filterFunction(rgbNumbers);
+            rgbString = rgbArrayToString(rgbNumbers);
+            image[r][c] = rgbString;
+        }
+    }
+}
 
 // TODO: Create filter functions
 function reddify(array){
     array[RED] = 255;
+}
+
+function blueify(array){
+    array[BLUE] = 255;
+}
+
+function greenify(array){
+    array[GREEN] = 255;
+}
+
+function decreaseBlue(array){
+    var blue = array[BLUE];
+    var adjBlue = blue - 100;
+    var endBlue = Math.max(0, adjBlue);
+    array[BLUE] = endBlue;
+}
+
+function increaseGreenByBlue(array){
+    var blue = array[BLUE];
+    var green = array[GREEN];
+    var adjGreen = blue + green;
+    var endGreen = Math.min(adjGreen, 255);
+    array[GREEN] = endGreen;
 }
 
 // CHALLENGE code goes below here
